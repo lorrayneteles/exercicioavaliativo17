9cdb142c347811cb86d0ee338b5fdcd2b1859808
lorrayneteles <lorrayneteles1305@gmail.com>
2024-10-16 18:43:51 -0300

    Add files via upload

---
+++ exercicioavaliativo17.py
@@ -0,0 +1,9 @@
+#Calcular a velocidade média
+
+distancia = int(input("Digite a distância percorrida:"))
+
+tempo = int(input("Digite o tempo gasto:"))
+
+velocidade_media = distancia / tempo
+
+print(f'A velocidade média é igual a:', velocidade_media,'km/h')
